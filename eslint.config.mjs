850f22cb34a47d860d2149358839a777b5b3e153
Tanveer Wahid <tan@wahid.email>
2026-08-31 01:19:18 -0700

    chore: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,17 @@
 import { defineConfig } from "eslint/config";
 import base from "./eslint/base.mjs";
 
-export default defineConfig(
-	...base,
-	{
-		files: ["**/*.ts"],
-		languageOptions: {
-			parserOptions: {
-				projectService: {
-					allowDefaultProject: ["vitest.config.ts"],
-				},
-				tsconfigRootDir: import.meta.dirname,
+export default defineConfig(...base, {
+	files: ["**/*.ts"],
+	languageOptions: {
+		parserOptions: {
+			projectService: {
+				allowDefaultProject: ["vitest.config.ts"],
 			},
-		},
-		rules: {
-			"@typescript-eslint/no-empty-function": ["error", { allow: ["private-constructors"] }],
+			tsconfigRootDir: import.meta.dirname,
 		},
 	},
-);
+	rules: {
+		"@typescript-eslint/no-empty-function": ["error", { allow: ["private-constructors"] }],
+	},
+});
